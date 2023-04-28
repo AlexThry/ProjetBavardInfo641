27334c03b2704a7a393512cc337b9b422da34bbd
Alexis Thierry <alexist103@gmail.com>
2023-04-28 10:15:57 +0200

    changement arbo

---
+++ Bavard/src/fr/proj/Papotage.java
@@ -0,0 +1,21 @@
+package fr.proj;
+
+public class Papotage {
+    private String sujet;
+    private String corps;
+
+    public Papotage(String sujet, String corps) {
+        this.sujet = sujet;
+        this.corps = corps;
+    }
+
+    public String getSujet() {
+        return this.sujet;
+    }
+
+    public String getCorps() {
+        return corps;
+    }
+
+
+}
---
+++ Bavard/src/fr/proj/PapotageEvent.java
@@ -0,0 +1,9 @@
+package fr.proj;
+
+public class PapotageEvent {
+    private Papotage papotage;
+
+    public PapotageEvent(Papotage papotage) {
+        this.papotage = papotage;
+    }
+}
