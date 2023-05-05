a8bd216f36e3da096de51bdc4dec1aa1e439da1f
Alexis Thierry <alexist103@gmail.com>
2023-05-05 15:48:28 +0200

    merge

--- Bavard/src/fr/proj/listeners/EcouteurBoutonValiderMessagerie.java
+++ Bavard/src/fr/proj/listeners/EcouteurBoutonValiderMessagerie.java
@@ -20,14 +20,6 @@ public class EcouteurBoutonValiderMessagerie implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-<<<<<<< HEAD
-        this.fenetre.removeMessages();
-        this.selected = fenetre.getBatiment().getBavards().get(this.comboBox.getSelectedIndex());
-        if (selected.getMessagesList().size() > 0) {
-            for (Message message: selected.getMessagesList()) {
-                System.out.println(message);
-                fenetre.addMessage(message.getSubject(), message.getSender(), message.getContent());
-=======
         if (fenetre.getBatiment().getBavards().size() == 0){
             JOptionPane.showMessageDialog(fenetre, "Vous devez créer un utilisateur pour pouvoir consulter les messages...", "Erreur", JOptionPane.ERROR_MESSAGE);
         }
@@ -35,10 +27,9 @@ public class EcouteurBoutonValiderMessagerie implements ActionListener {
             this.selected = fenetre.getBatiment().getBavards().get(this.comboBox.getSelectedIndex());
             this.fenetre.removeMessages();
             if (selected.getMessagesList().size() > 0) {
-                for (Message message: selected.getMessagesList()) {
-                    fenetre.addMessage(message.getSubject(), message.getSender(), message.getContent());
+                for (Message message2: selected.getMessagesList()) {
+                    fenetre.addMessage(message2.getSubject(), message2.getSender(), message2.getContent());
                 }
->>>>>>> 59525f6f0fef226bbb5ab819d81fe234b7e9b2cd
             }
         }
     }
